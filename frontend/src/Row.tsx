07273ff24d39b3418c4bea9a590be1d53f343ca8
Luominai <kevinthepear@gmail.com>
2026-06-09 13:00:19 -0400

    frontend stuff

--- frontend/src/Row.tsx
+++ frontend/src/Row.tsx
@@ -6,8 +6,19 @@ interface RowProps {
 
 export default function Row({character}: RowProps) {
     return (
-        <div>
+        <div style={{
+            display: "flex",
+            gap: "100px",
+            border: "1px solid black",
+            height: "100px",
+            justifyContent: "space-around"
+        }}>
             <img src={character.image}></img>
+            <div>{character.name}</div>
+            <div>{character.rarity}</div>
+            <div>{character.afflatus}</div>
+            <div>{character.damage}</div>
+            <div>{character.tags}</div>
         </div>
     )
 }
